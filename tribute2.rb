c4f56917ebfe2b21cbce46b15d7b049eed2dc2f2
Bente Schopman <b.l.schopman@gmail.com>
2021-02-27 15:19:59 +0100

    Sonic Pi made music

---
+++ tribute2.rb
@@ -0,0 +1,94 @@
+# Welcome to Sonic Pi
+sample_one = :drum_heavy_kick
+sample_two = :bass_trance_c
+sample_three = :bass_voxy_hit_c
+synth_one = :growl
+synth_two = :dsaw
+
+
+10.times do
+  use_synth :rodeo
+  sleep 0.33333333334
+  sample sample_one
+  sleep 0.5
+  use_synth synth_one
+  play 40, release:0.5
+  sleep 0.25
+end
+
+in_thread do
+  loop do
+    sample sample_one
+    play 25, note: 50, release:0.10
+    sleep 0.5
+    use_synth synth_one
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+in_thread do
+  loop do
+    sample sample_two
+    play 25, note: 50, release:0.10
+    sleep 0.5
+    use_synth synth_two
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+in_thread do
+  loop do
+    sample sample_three
+    play 25, note: 50, release:0.10
+    sleep 0.5
+    use_synth synth_one
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+in_thread do
+  loop do
+    sample sample_three
+    play 25, note: 50, release:0.10
+    sleep 0.5
+    use_synth synth_two
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+in_thread do
+  loop do
+    sample sample_one
+    play 25, note: 50, release:0.10
+    sleep 0.5
+  end
+end
+in_thread do
+  10.times do
+    use_synth :rodeo
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+in_thread do
+  10.times do
+    sample sample_two
+    play 60, note: 25, release:0.75
+    sleep 0.5
+    use_synth synth_two
+    play 50, note:15, release:0.25
+    sleep 0.25
+  end
+end
+in_thread do
+  10.times do
+    sample sample_three
+    play 25, note: 20, release:0.20
+    use_synth synth_one
+    play 40,note: 10,  release:0.5
+    sleep 0.25
+  end
+end
+
+
+
